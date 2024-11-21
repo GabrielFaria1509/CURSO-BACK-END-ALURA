556af7d9843e87b84a3cbe7798f69575140e469a
GabrielFaria1509 <fariagabriel1509@gmail.com>
2024-11-21 18:40:12 -0300

    Update postsControllers.js

--- postsControllers.js
+++ postsControllers.js
@@ -1,4 +1,5 @@
-import getTodosPosts from "../models/postsModels.js";
+import {getTodosPosts , criarPost} from "../models/postsModels.js";
+import fs from "fs"
 
 export async function listarPosts(req, res)  {
     // Chama a função getTodosPosts para obter os posts do banco de dados.
@@ -6,6 +7,40 @@ export async function listarPosts(req, res)  {
     // Envia uma resposta HTTP com status 200 (OK) e os posts no formato JSON.
     res.status(200).json(posts);
     } 
+export async function postarNovoPost(req,res){
+        const novoPost = req.body;
+        try{
+            const postCriado = await criarPost(novoPost);
+            res.status(200).json(postCriado);
+        } catch(erro){         //o quie fazer se der errado a criacão do post
+            console.error(erro);
+            res.status(500).json({"Eroo" :"Falha na requisicao "})
+        }
+
+}
+
+export async function uploadImagem(req,res){
+    const novoPost = {
+        descricao : "",
+        imgUrl : req.file.originalname,
+        alt : ""
+    };
+
+
+    try{
+        const postCriado = await criarPost(novoPost);
+        const imagemAtualziada = `uploads/${postCriado.insertedId}.png`
+        fs.renameSync(req.file.path , imagemAtualziada)
+        res.status(200).json(postCriado);
+    } catch(erro){         //o quie fazer se der errado a criacão do post
+        console.error(erro);
+        res.status(500).json({"Eroo" :"Falha na requisicao "})
+    }
+
+}
+
+
+
     
     
  
