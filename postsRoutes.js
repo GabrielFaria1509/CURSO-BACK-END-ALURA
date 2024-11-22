fe8762d7dd4c669dde2f1eb8653e1a8dd839dc66
GabrielFaria1509 <fariagabriel1509@gmail.com>
2024-11-22 17:53:46 -0300

    Update postsRoutes.js

--- postsRoutes.js
+++ postsRoutes.js
@@ -1,40 +1,45 @@
 import express from "express";
 import multer from "multer";
-import { listarPosts , postarNovoPost , uploadImagem } from "../controllers/postsControllers.js";    //back end que usa .js 
+import cors from "cors";
+import { listarPosts , postarNovoPost , uploadImagem , atualizarNovoPost } from "../controllers/postsControllers.js";    //back end que usa .js 
 
-// Configura o armazenamento das imagens em disco, utilizando o multer.diskStorage. 
-// Essa configuração é específica para sistemas Windows, garantindo que o caminho para as imagens seja corretamente definido.
-const storage = multer.diskStorage({               
-    destination: function (req, file, cb) {
-        // Define o diretório de destino para as imagens.
-        cb(null, 'uploads/');
-    },
-    filename: function (req, file, cb) {
-        // Define o nome do arquivo, mantendo o nome original.
-        cb(null, file.originalname);
-    }
+const corsoptions = {
+    origin: "http://localhost:8000",
+    optionsSuccessStatus : 200
+}
+
+const storage = multer.diskStorage({              //trechoi necessário para usuários dw windows
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
 })
 
-// Cria uma instância do multer, configurando o destino dos arquivos e o armazenamento.
-// Essa configuração é específica para sistemas Windows.
 const upload = multer({ dest: "./uploads" , storage})
 
-// Para sistemas Linux ou macOS, a configuração seria mais simples:
-// const upload = multer({ dest: "./uploads" })
+//linux ou mac
+//const upload = multer({ dest: "./uploads" })
 
 const routes = (app) => {
 
-    // Habilita o middleware JSON para que o Express possa interpretar requisições com corpo no formato JSON.
-    app.use(express.json());
+    // Habilita o middleware JSON para que o Express possa interpretar requisições com corpo no formato JSON.
+    app.use(express.json());
+    app.use(cors(corsoptions));
 
-    // Rota GET para obter todos os posts.
-    app.get("/posts",listarPosts);
-    
-    // Rota para enviar um novo post.
-    app.post("/posts",postarNovoPost)
-    // Rota para fazer upload de uma imagem, utilizando o middleware multer.
-    // O parâmetro "imagem" indica o nome do campo no formulário HTML.
-    app.post("/upload",upload.single("imagem"),uploadImagem)
+    // Rota GET para obter todos os posts.
+    app.get("/posts",listarPosts);
+    
+    //rota para enviar um post
+    app.post("/posts",postarNovoPost)
+    
+    app.post("/upload",upload.single("imagem"),uploadImagem)//chama a função controladora para processamento da imagem
+   
+    //rota para upload de imagens("assumindo" uma única imagem chamad imagem por vez)
+    app.put("/upload/:id",atualizarNovoPost)
 }
 
 export default routes;
+
+  
